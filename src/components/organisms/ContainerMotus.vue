a4dee129cc03f9c2316aa642e3afc2bc868d9900
Simon Gioffredi <simon@spin-interactive.com>
2024-06-01 14:58:40 +0200

    :sparkles: End motus basic feature

--- src/components/organisms/ContainerMotus.vue
+++ src/components/organisms/ContainerMotus.vue
@@ -2,12 +2,13 @@
 import TheTitle from '@/components/atoms/TheTitle.vue'
 import TheSection from '@/components/atoms/TheSection.vue'
 import useWord from '@/composables/use-word'
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
 
 const word = useWord()
 const guess = ref('')
 const attempts = ref([])
 const message = ref('')
+const isOver = ref(false)
 
 const placeholder = computed(() => {
   return `Entrez un mot de ${word.value.length} lettres`
@@ -20,29 +21,53 @@ const onSubmit = () => {
   attempts.value.push({ id: attempts.value.length, word: guessLowerCase })
 
   if (guessLowerCase === word.value) {
-    message.value = "Félicitations ! Vous avez trouvé le mot !";
-    return;
+    message.value = 'Félicitations ! Vous avez trouvé le mot !'
+    return
   }
 
   if (attempts.value.length >= 6) {
-    message.value = `Désolé, vous avez utilisé toutes vos tentatives. Le mot était : ${word.value}`;
+    message.value = `Désolé, vous avez utilisé toutes vos tentatives. Le mot était : ${word.value}`
   }
 
-  guess.value = '';
+  guess.value = ''
 }
+
+const getClass = (letter, index) => {
+  if (letter === word.value[index]) {
+    return 'good'
+  } else if (word.value.includes(letter)) {
+    return 'bad-placement'
+  } else {
+    return 'false'
+  }
+}
+
+watch(message, () => {
+  isOver.value = true
+})
 </script>
 
 <template>
   <TheSection class="container-motus">
-    <TheTitle title="Motus" />
+    <TheTitle :title="`Motus - ${word}`" />
     <input
       v-model="guess"
       :maxlength="word.length"
       @keyup.enter="onSubmit"
       :placeholder="placeholder"
+      :disabled="isOver"
     >
-    <p>Le mot est : {{ word }}</p>
-    <p>Les essais : {{ attempts }}</p>
+    <ul v-for="attempt in attempts" :key="attempt.id">
+      <li>
+        <p>
+          <span v-for="(letter, i) in attempt.word" :key="i" :class="getClass(letter, i)">
+            {{ letter }}
+          </span>
+        </p>
+      </li>
+    </ul>
+
+    <div v-if="message">{{ message }}</div>
   </TheSection>
 </template>
 
